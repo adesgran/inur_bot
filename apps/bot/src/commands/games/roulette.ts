6e564a4d526132427a992011c0b3c6bbe275a5b9
adesgran <adesgran.dev@gmail.com>
2024-08-08 00:40:56 +0200

    Fix roulette

--- apps/bot/src/commands/games/roulette.ts
+++ apps/bot/src/commands/games/roulette.ts
@@ -4,7 +4,7 @@ import { BotCommandContext } from '@twurple/easy-bot/lib';
 export const rouletteCommandName: string = 'roulette';
 
 export const rouletteHandler: (params: string[], context: BotCommandContext, messageQueue: MessageQueue) => Promise<void> = async (params: string[], context: BotCommandContext, messageQueue: MessageQueue) => {
-    await messageQueue.reply(context.userName, `Sorry ${context.userName}, this command is not working for the moment (Cooldown 30s)`, context.msg);
+    await messageQueue.reply(context.broadcasterName, `Sorry ${context.userName}, this command is not working for the moment (Cooldown 30s)`, context.msg);
 };
 
 export const rouletteOptions: { globalCooldown?: number } = {
